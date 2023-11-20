d660e1e4c0c6cabd200f84268e05d718e097c768
1aal <alal@zju.edu.cn>
2023-11-20 15:59:23 +0800

    add index UT

---
+++ pkg/cmd/addon/index_test.go
@@ -0,0 +1,80 @@
+package addon
+
+import (
+	"bytes"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/cli-runtime/pkg/genericiooptions"
+)
+
+var _ = Describe("index test", func() {
+	var streams genericiooptions.IOStreams
+	var out *bytes.Buffer
+	const (
+		defaultIndexName = "kubeblocks"
+		testIndexName    = "kb-other"
+		testIndexURL     = "unknown"
+	)
+	BeforeEach(func() {
+		streams, _, out, _ = genericiooptions.NewTestIOStreams()
+	})
+	It("test index cmd", func() {
+		Expect(newIndexCmd(streams)).ShouldNot(BeNil())
+	})
+
+	It("test index add cmd", func() {
+		cmd := newIndexAddCmd()
+		Expect(cmd).ShouldNot(BeNil())
+		Expect(addIndex([]string{defaultIndexName, testIndexURL})).Should(HaveOccurred())
+		Expect(addIndex([]string{testIndexName, testIndexURL})).Should(HaveOccurred())
+	})
+
+	It("test index delete cmd", func() {
+		Expect(newIndexDeleteCmd()).ShouldNot(BeNil())
+		Expect(deleteIndex(testIndexName)).Should(HaveOccurred())
+	})
+
+	It("test index list cmd", func() {
+		Expect(newIndexListCmd(streams)).ShouldNot(BeNil())
+
+		Expect(listIndexes(out)).Should(Succeed())
+		expect := `INDEX        URL                                           
+kubeblocks   https://github.com/apecloud/block-index.git   
+`
+		Expect(out.String()).Should(Equal(expect))
+	})
+
+	It("test index update cmd", func() {
+		Expect(newIndexUpdateCmd(streams)).ShouldNot(BeNil())
+
+		o := &updateOption{
+			names:     make([]string, 0),
+			all:       false,
+			IOStreams: streams,
+		}
+		Expect(o.validate([]string{defaultIndexName})).Should(Succeed())
+		Expect(o.validate([]string{testIndexName})).Should(HaveOccurred())
+		Expect(o.validate([]string{})).Should(HaveOccurred())
+		o.all = true
+		Expect(o.validate([]string{})).Should(Succeed())
+		Expect(o.run()).Should(Succeed())
+	})
+
+	It("test index name", func() {
+		cases := []struct {
+			name    string
+			success bool
+		}{
+			{"kubeblocks", true}, {"KubeBlocks123", true}, {"Kube_Blocks", true}, {"kube-blocks", true}, {"12345", true},
+			{"kube blocks", false}, {"kube@blocks", false}, {"", false}, {"kubekubekubeblocks", false},
+		}
+
+		for _, t := range cases {
+			if t.success {
+				Expect(IsValidIndexName(t.name)).Should(BeTrue())
+			} else {
+				Expect(IsValidIndexName(t.name)).Should(BeFalse())
+			}
+		}
+	})
+})
