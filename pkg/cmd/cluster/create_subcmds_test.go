b343ac51ff530b7c8358e7fa4a4244e0547754b8
wangyelei <yelei.wang@apecloud.com>
2024-10-23 09:17:23 +0800

    fix make test bug

--- pkg/cmd/cluster/create_subcmds_test.go
+++ pkg/cmd/cluster/create_subcmds_test.go
@@ -128,7 +128,7 @@ var _ = Describe("create cluster by cluster type", func() {
 		Expect(o.Run()).Should(Succeed())
 	})
 
-	FIt("create sharding cluster command", func() {
+	It("create sharding cluster command", func() {
 		By("create commands")
 		cmds := buildCreateSubCmds(createOptions)
 		Expect(cmds).ShouldNot(BeNil())
