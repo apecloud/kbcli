f15a72be444915ac74e192b75d2b48d850523285
Shanshan <shanshan.ying@apecloud.com>
2026-05-27 14:59:18 +0800

    feat: tidy preflight internal types (#678)

--- pkg/preflight/concat_spec_test.go
+++ pkg/preflight/concat_spec_test.go
@@ -25,7 +25,7 @@ import (
 
 	"k8s.io/apimachinery/pkg/util/yaml"
 
-	preflightv1beta2 "github.com/apecloud/kubeblocks/externalapis/preflight/v1beta2"
+	troubleshoot "github.com/replicatedhq/troubleshoot/pkg/apis/troubleshoot/v1beta2"
 )
 
 var _ = Describe("concat_spec_test", func() {
@@ -74,8 +74,8 @@ spec:
              message: The redis server must be at least version 5
          - pass:
              message: The redis connection checks out.`
-		targetSpec := new(preflightv1beta2.Preflight)
-		sourceSpec := new(preflightv1beta2.Preflight)
+		targetSpec := new(troubleshoot.Preflight)
+		sourceSpec := new(troubleshoot.Preflight)
 		Expect(yaml.Unmarshal([]byte(targetByte), targetSpec)).Should(Succeed())
 		Expect(yaml.Unmarshal([]byte(sourceByte), sourceSpec)).Should(Succeed())
 		var newSpec = ConcatPreflightSpec(nil, sourceSpec)
@@ -84,93 +84,5 @@ spec:
 		Expect(newSpec).Should(Equal(targetSpec))
 		newSpec = ConcatPreflightSpec(targetSpec, sourceSpec)
 		Expect(len(newSpec.Spec.Analyzers)).Should(Equal(2))
-	})
-	It("ConcatHostPreflightSpec Test", func() {
-		targetByte := `
-apiVersion: troubleshoot.sh/v1beta2
-kind: HostPreflight
-metadata:
- name: cpu
-spec:
- collectors:
-   - cpu: {}
- analyzers:
-   - cpu:
-       outcomes:
-         - fail:
-             when: "physical < 4"
-             message: At least 4 physical CPU cores are required
-         - fail:
-             when: "logical < 8"
-             message: At least 8 CPU cores are required
-         - warn:
-             when: "count < 16"
-             message: At least 16 CPU cores preferred
-         - pass:
-             message: This server has sufficient CPU cores.`
-		sourceByte := `
-apiVersion: troubleshoot.sh/v1beta2
-kind: HostPreflight
-metadata:
- name: http
-spec:
- collectors:
-   - http:
-       collectorName: registry
-       get:
-         url: https://registry.replicated.com
- analyzers:
-   - http:
-       collectorName: registry
-       outcomes:
-         - fail:
-             when: "error"
-             message: Error connecting to registry
-         - pass:
-             when: "statusCode == 404"
-             message: Connected to registry
-         - fail:
-             message: "Unexpected response"`
-		targetSpec := new(preflightv1beta2.HostPreflight)
-		sourceSpec := new(preflightv1beta2.HostPreflight)
-		Expect(yaml.Unmarshal([]byte(targetByte), targetSpec)).Should(Succeed())
-		Expect(yaml.Unmarshal([]byte(sourceByte), sourceSpec)).Should(Succeed())
-		var newSpec = ConcatHostPreflightSpec(nil, sourceSpec)
-		Expect(newSpec).Should(Equal(sourceSpec))
-		newSpec = ConcatHostPreflightSpec(targetSpec, nil)
-		Expect(newSpec).Should(Equal(targetSpec))
-		newSpec = ConcatHostPreflightSpec(targetSpec, sourceSpec)
-		Expect(len(newSpec.Spec.Analyzers)).Should(Equal(2))
-	})
-
-	It("ExtractHostPreflightSpec Test", func() {
-		sourceByte := `
-apiVersion: troubleshoot.sh/v1beta2
-kind: HostPreflight
-metadata:
- name: http
-spec:
- collectors:
-   - http:
-       collectorName: registry
-       get:
-         url: https://registry.replicated.com
- analyzers:
-   - http:
-       collectorName: registry
-       outcomes:
-         - fail:
-             when: "error"
-             message: Error connecting to registry
-         - pass:
-             when: "statusCode == 404"
-             message: Connected to registry
-         - fail:
-             message: "Unexpected response"`
-		sourceSpec := new(preflightv1beta2.HostPreflight)
-		Expect(yaml.Unmarshal([]byte(sourceByte), sourceSpec)).Should(Succeed())
-		Expect(ExtractHostPreflightSpec(nil)).Should(BeNil())
-		newSpec := ExtractHostPreflightSpec(sourceSpec)
-		Expect(len(newSpec.Spec.Collectors)).Should(Equal(1))
 	})
 })
